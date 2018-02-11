1f70b8014e0884284178c56bcff440e87b3f6823
Joseph Jarriel <josephjarriel@gmail.com>
2018-02-10 22:01:55 -0500

    new format for bio, services content, updated notes, contact form email confirmation

--- js/contact.js
+++ js/contact.js
@@ -9,8 +9,9 @@ $(document).ready(function() {
         var firstName = event.srcElement[0].value;
         var lastName = event.srcElement[1].value;
         var email = event.srcElement[2].value;
-        var comment = event.srcElement[3].value;
-        var date = event.srcElement[4].value;
+        var emailConfirm = event.srcElement[3].value;
+        var comment = event.srcElement[4].value;
+        var date = event.srcElement[5].value;
 
         console.log(firstName + lastName + email + comment + date);
         console.log($("#lastName"));
@@ -34,7 +35,7 @@ $(document).ready(function() {
     });
 });
 
-function check(input) {
+function checkTest(input) {
     if (input.value == "good" ||
         input.value == "fine" ||
         input.value == "tired") {
@@ -42,6 +43,16 @@ function check(input) {
    } 
    else if ($("#lastName")[0].value != input.value) {
        input.setCustomValidity("This value doesn't match the last name!");
+   }
+   else {
+      // input is fine -- reset the error message
+      input.setCustomValidity("");
+    }
+  }
+
+function checkEmail(input) {
+    if ($("#email")[0].value != input.value) {
+       input.setCustomValidity("Doesn't match the email");
    }
    else {
       // input is fine -- reset the error message
